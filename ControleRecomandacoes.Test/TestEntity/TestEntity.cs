02f0e3b8ee2ba1e70f4bc6988c9ec658faa5d2d6
RaMadaSilva <raul01mateia@gmail.com>
2023-05-03 18:29:10 +0100

    tests red

---
+++ ControleRecomandacoes.Test/TestEntity/TestEntity.cs
@@ -0,0 +1,54 @@
+namespace ControleRecomandacoes.Test.TestEntity;
+
+[TestClass]
+public class TestEntity
+{
+    [TestMethod]
+    [TestCategory("domain")]
+    public void SolicaitarRecomendacaoValida()
+    {
+        Assert.Fail();
+    }
+
+    [TestMethod]
+    [TestCategory("domain")]
+    public void ReceberUmaRecomendandacaoValida()
+    {
+        Assert.Fail();
+    }
+
+    [TestMethod]
+    [TestCategory("domain")]
+    public void ReceberUmaRecomendandacaoInvalida()
+    {
+        Assert.Fail();
+    }
+
+    [TestMethod]
+    [TestCategory("domain")]
+    public void DevolverUmaRecomendandacaoRecebida()
+    {
+        Assert.Fail();
+    }
+
+    [TestMethod]
+    [TestCategory("domain")]
+    public void DevolverUmaRecomendandacaoExpedida()
+    {
+        Assert.Fail();
+    }
+
+    [TestMethod]
+    [TestCategory("domain")]
+    public void DadaUmaCartaDeRecomendacaoValidaAlterarEstadoParaDevolvido()
+    {
+        Assert.Fail();
+    }
+
+    [TestMethod]
+    [TestCategory("domain")]
+    public void DadaUmaCartaDeRecomendacaoValidaAlterarEstadoParaInvalido()
+    {
+        Assert.Fail();
+    }
+}
