472cccfcf6c17fc241ad1b9c681d650cf3b0183e
RaMadaSilva <raul01mateia@gmail.com>
2023-05-18 21:54:05 +0100

    Montando a classe de Testes com os metodos todos falhando

---
+++ ControleRecommads.Test/HandlerTest/IssuedHandlerTeste.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace ControleRecommads.Test.HandlerTest
+{
+    [TestClass]
+    public class IssuedHandlerTeste
+    {
+        [TestMethod]
+        [TestCategory("Handler")]
+        public void Dado_Comando_Valido_Solicitar_Uma_Carta()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        [TestCategory("Handler")]
+        public void Dado_Um_Repositorio_Vericar_Se_Existe_Carta_Valida()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        [TestCategory("Handler")]
+        public void Dada_Uma_Carta_Valida_Solicitar_Outra_Carta()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        [TestCategory("Handler")]
+        public void Dada_uma_Carta_Invalida_Solicitar_Outra_Carta()
+        {
+            Assert.Fail();
+        }
+        [TestMethod]
+        [TestCategory("Handler")]
+        public void Dado_Um_Comando_Invalido_Solicitar_Uma_Carta()
+        {
+            Assert.Fail();
+        }
+
+    }
+}
