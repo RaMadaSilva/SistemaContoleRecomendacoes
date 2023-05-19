15ec033a523125aa62a4f25b5dbeff64e5ee5ba0
RaMadaSilva <raul01mateia@gmail.com>
2023-05-19 17:38:46 +0100

    terminando o Received Fake

--- ControleRecommads.Test/FakeRepositories/FakeReceivedRepository.cs
+++ ControleRecommads.Test/FakeRepositories/FakeReceivedRepository.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using ControleRecommads.Domain.Entities;
 using ControleRecommads.Domain.Entities.Enums;
 using ControleRecommads.Domain.Entities.ValueObject;
@@ -14,37 +10,34 @@ namespace ControleRecommads.Test.FakeRepositories
         List<ReceivedRecommendation> receivedRecommendations = new()
         {
             new ReceivedRecommendation(new("Nelma",  "Kiffen", 111111111, "Maianga"), new DateTime(2023, 05, 19), new("Maianga", "Maianga")),
-            new ReceivedRecommendation(new("Walter",  "Msamo", 222222222, "Maculusso"), new DateTime(2023, 05, 19), new("Central", "Maculusso")),
-            new ReceivedRecommendation(new("Paulo",  "Pimenta", 333333333, "Rainha Jinga"), new DateTime(2023, 05, 19), new("Ilha", "Ilha")),
+            new ReceivedRecommendation(new("Walter",  "Msamo", 222222222, "Maculusso"), new DateTime(2022, 05, 19), new("Central", "Maculusso")),
+            new ReceivedRecommendation(new("Paulo",  "Pimenta", 333333333, "Rainha Jinga"), new DateTime(2021, 05, 19), new("Ilha", "Ilha")),
             new ReceivedRecommendation(new("Daniela",  "Flor", 444444444, "Bairro Popular"), new DateTime(2023, 05, 19), new("Popula", "Popula")),
             new ReceivedRecommendation(new("Henrique",  "Sebastião", 555555555, "Zango"), new DateTime(2023, 05, 19), new("Zango", "Zango")),
             new ReceivedRecommendation(new("Feliciana",  "João", 666666666, "Prenda"), new DateTime(2023, 05, 19), new("Prenda", "Prenda")),
-            new ReceivedRecommendation(new("Helena",  "João", 777777777, "Cazenga"), new DateTime(2023, 05, 19), new("Central do Cazenga", "Cazenga")),
+            new ReceivedRecommendation(new("Helena",  "João", 777777777, "Cazenga"), new DateTime(1995, 05, 19), new("Central do Cazenga", "Cazenga")),
 
         };
         public IList<ReceivedRecommendation> GetAllReceivedRecommendation()
-        {
-            throw new NotImplementedException();
-        }
+            => receivedRecommendations;
 
         public IList<ReceivedRecommendation> GetAllReceivedRecommendationWithState(ERecommendationState state)
-        {
-            throw new NotImplementedException();
-        }
+            => receivedRecommendations.Where(x => x.State == state).ToList();
 
         public ReceivedRecommendation GetRecommendation(Guid id)
-        {
-            throw new NotImplementedException();
-        }
-
+            => receivedRecommendations.Where(x => x.Id == id).FirstOrDefault();
         public ReceivedRecommendation GetRecommendationValid(Member member)
         {
-            throw new NotImplementedException();
+            var receivedRecommendation = receivedRecommendations
+                    .Where(x => x.State == ERecommendationState
+                    .valido)
+                    .ToList()
+                    .Find(x => x.Member == member);
+            if (receivedRecommendation != null)
+                return receivedRecommendation;
+            return null;
         }
-
         public void Save(ReceivedRecommendation recommendation)
-        {
-            throw new NotImplementedException();
-        }
+            => receivedRecommendations.Add(recommendation);
     }
 }
