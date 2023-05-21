900999da8725522c73813b3fd154ac247711f03a
RaMadaSilva <raul01mateia@gmail.com>
2023-05-22 00:27:36 +0100

    implementando o metodo de update no Fake Received

--- ControleRecommads.Test/FakeRepositories/FakeReceivedRepository.cs
+++ ControleRecommads.Test/FakeRepositories/FakeReceivedRepository.cs
@@ -13,8 +13,8 @@ namespace ControleRecommads.Test.FakeRepositories
             new ReceivedRecommendation(new("Walter",  "Msamo", 222222222, "Maculusso"), new DateTime(2022, 05, 19), new("Central", "Maculusso")),
             new ReceivedRecommendation(new("Paulo",  "Pimenta", 333333333, "Rainha Jinga"), new DateTime(2021, 05, 19), new("Ilha", "Ilha")),
             new ReceivedRecommendation(new("Daniela",  "Flor", 444444444, "Bairro Popular"), new DateTime(2023, 05, 19), new("Popula", "Popula")),
-            new ReceivedRecommendation(new("Henrique",  "Sebastião", 555555555, "Zango"), new DateTime(2023, 05, 19), new("Zango", "Zango")),
-            new ReceivedRecommendation(new("Feliciana",  "João", 666666666, "Prenda"), new DateTime(2023, 05, 19), new("Prenda", "Prenda")),
+            new ReceivedRecommendation(new("Henrique",  "Sebastião", 555555555, "Zango"), new DateTime(2023, 04, 19), new("Zango", "Zango")),
+            new ReceivedRecommendation(new("Feliciana",  "João", 666666666, "Prenda"), new DateTime(2023, 02, 19), new("Prenda", "Prenda")),
             new ReceivedRecommendation(new("Helena",  "João", 777777777, "Cazenga"), new DateTime(1995, 05, 19), new("Central do Cazenga", "Cazenga")),
 
         };
@@ -41,7 +41,13 @@ namespace ControleRecommads.Test.FakeRepositories
 
         public void UpdateRecommendation(ReceivedRecommendation recommendation)
         {
-            //TODO
+            var received = receivedRecommendations.Where(x => x.Member == recommendation.Member).FirstOrDefault();
+
+            if (received != null && received.State == ERecommendationState.valido)
+            {
+                receivedRecommendations.Remove(received);
+                receivedRecommendations.Add(recommendation);
+            }
         }
     }
 }
