961b3ce2ee6da40e983e51f6a6a4c5f59fffa8b1
RaMadaSilva <raul01mateia@gmail.com>
2023-05-21 16:22:31 +0100

    alterando o estado de uma carta de recomendacao recebida de valido para devolvido

--- ControleRecommads.Domain/Handler/ReceivedRecommendationHandler.cs
+++ ControleRecommads.Domain/Handler/ReceivedRecommendationHandler.cs
@@ -1,6 +1,7 @@
 using ControleRecommads.Domain.Commands;
 using ControleRecommads.Domain.Commands.Interfaces;
 using ControleRecommads.Domain.Entities;
+using ControleRecommads.Domain.Entities.Enums;
 using ControleRecommads.Domain.Entities.ValueObject;
 using ControleRecommads.Domain.Handler.Interface;
 using ControleRecommads.Domain.Repositories;
@@ -8,7 +9,7 @@ using Flunt.Notifications;
 
 namespace ControleRecommads.Domain.Handler
 {
-    public class ReceivedRecommendationHandler : Notifiable<Notification>, IHandler<ReceiveCommand>
+    public class ReceivedRecommendationHandler : Notifiable<Notification>, IHandler<ReceiveCommand>, IHandler<RetornRecommendationCommand>
     {
         private readonly IRecommendationRepository<ReceivedRecommendation> _repostory;
 
@@ -44,6 +45,24 @@ namespace ControleRecommads.Domain.Handler
             //4# retornar o resultado 
 
             return new CommandResult(true, "Recomendação salva com sucesso", received);
+        }
+
+        public ICommandResult Handler(RetornRecommendationCommand command)
+        {
+            //pegar a carta do banco de dedos
+            var recommendation = _repostory.GetRecommendation(command.Id);
+
+            if (recommendation == null || recommendation.State != ERecommendationState.valido)
+            {
+                AddNotification("IssuedRecommendation", "Recomendação Invalida ou inexistente");
+                return new CommandResult(false, "Não foi possivel actualizar a recomendação", Notifications);
+            }
+
+            //Caso Exista a recomendação e o seu estado seja valida
+            recommendation.UpdateStateDevolvido(command.RetornDate);
+            _repostory.UpdateRecommendation(recommendation);
+
+            return new CommandResult(true, "Recomendação actualizada com sucesso", recommendation);
         }
     }
 }
