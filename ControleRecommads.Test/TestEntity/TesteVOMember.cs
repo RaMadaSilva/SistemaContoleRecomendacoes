3f76988d2662442542facfee3d765090cd5be3a0
RaMadaSilva <raul01mateia@gmail.com>
2023-05-23 22:33:33 +0100

    Realizado os testes do valueObject

--- ControleRecommads.Test/TestEntity/TesteVOMember.cs
+++ ControleRecommads.Test/TestEntity/TesteVOMember.cs
@@ -2,18 +2,26 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ControleRecommads.Domain.Entities.ValueObject;
 
 namespace ControleRecommads.Test.TestEntity
 {
     [TestClass]
     public class TesteVOMember
     {
+        private readonly Member membro_1 = new Member("Raul", "Silva", 11111);
+        private readonly Member member_2 = new Member("Raul", "Silva", 11111);
+        private readonly Member member_3 = new Member("Jandira", "SIlva", 222222);
+
         [TestMethod]
         [TestCategory("ValueObject")]
         public void Dado_Dois_Membro_Vericar_Se_Sao_Iguais()
-        {
-            Assert.Fail();
-        }
+            => Assert.IsTrue(membro_1.Equals(member_2));
+
+        [TestMethod]
+        [TestCategory("ValueObject")]
+        public void Dado_Dois_Membros_Diferente_Verificar()
+            => Assert.IsFalse(membro_1.Equals(member_3));
 
     }
 }
