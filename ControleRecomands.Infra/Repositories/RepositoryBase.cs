ee38cb4d256a3b6f102b6f4fa459a0babbe1fb9b
RaMadaSilva <raul01mateia@gmail.com>
2023-06-11 14:06:42 +0100

    Implementado o Repositorio Base

---
+++ ControleRecomands.Infra/Repositories/RepositoryBase.cs
@@ -0,0 +1,52 @@
+using System.Linq.Expressions;
+using ControleRecomands.Infra.Context;
+using ControleRecommads.Domain.Entities;
+using ControleRecommads.Domain.Entities.ValueObject;
+using ControleRecommads.Domain.IRepositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace ControleRecomands.Infra.Repositories
+{
+    public class RepositoryBase<T> : IRepositoryBase<T> where T : Recommendation
+    {
+        private readonly RecommendationDbContext _context;
+        private readonly DbSet<T> _dbSet;
+        public RepositoryBase(RecommendationDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public void Create(T recommendation)
+        {
+            throw new NotImplementedException();
+        }
+        public IEnumerable<T> GetAllRecommendation()
+        {
+            return _dbSet.AsNoTracking();
+        }
+        public IEnumerable<T> GetAllRecommendationWithState(Expression<Func<T, bool>> predicate)
+        {
+            return _dbSet.Where(predicate);
+        }
+        public T GetRecommendation(Guid id)
+        {
+            var recommendation = _dbSet.Find(id);
+            if (recommendation is null)
+                return null;
+            return recommendation;
+        }
+        public T GetRecommendationValid(Member member)
+        {
+            var recommendation = _dbSet.Find(member);
+            if (recommendation is null)
+                return null;
+            return recommendation;
+        }
+
+        public void UpdateRecommendation(T recommendation)
+        {
+            _context.Entry(recommendation).State = EntityState.Modified;
+        }
+    }
+}
--- ControleRecommads.Domain/IRepositories/IRepositorioBase.cs
+++ ControleRecommads.Domain/IRepositories/IRepositorioBase.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Linq.Expressions;
 using ControleRecommads.Domain.Entities;
 using ControleRecommads.Domain.Entities.Enums;
 using ControleRecommads.Domain.Entities.ValueObject;
@@ -7,7 +9,7 @@ namespace ControleRecommads.Domain.IRepositories
     public interface IRepositoryBase<T> where T : Recommendation
     {
         IEnumerable<T> GetAllRecommendation();
-        IEnumerable<T> GetAllRecommendationWithState(ERecommendationState state);
+        IEnumerable<T> GetAllRecommendationWithState(Expression<Func<T, bool>> predicate);
         T GetRecommendation(Guid id);
         T GetRecommendationValid(Member member);
         void Create(T recommendation);
