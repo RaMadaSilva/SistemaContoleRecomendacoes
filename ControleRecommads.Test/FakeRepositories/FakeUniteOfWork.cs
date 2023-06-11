ba1be81d4badfbeaa7f895c81907904a2309aaae
RaMadaSilva <raul01mateia@gmail.com>
2023-06-11 18:55:11 +0100

    Criando a api

--- ControleRecommads.Test/FakeRepositories/FakeUniteOfWork.cs
+++ ControleRecommads.Test/FakeRepositories/FakeUniteOfWork.cs
@@ -1,13 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using ControleRecommads.Domain.Entities;
 using ControleRecommads.Domain.IRepositories;
 using ControleRecommads.Domain.IRepositories.IUniteOfWork;
 
 namespace ControleRecommads.Test.FakeRepositories
 {
+
     public class FakeUniteOfWork : IUniteOfWork
     {
         public IRepositoryBase<ReceivedRecommendation> ReceivedRecommendationRepository => throw new NotImplementedException();
---
+++ ControleRecommands.Api/Program.cs
@@ -0,0 +1,27 @@
+var builder = WebApplication.CreateBuilder(args);
+
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+var connection = builder.
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
