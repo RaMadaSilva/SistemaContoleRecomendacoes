4cd05fbc9970f01cbcaba62fd83841ae49847318
RaMadaSilva <raul01mateia@gmail.com>
2023-06-16 09:58:54 +0100

    Resolvendo a injecção de dependecia

--- ControleRecommands.Api/Program.cs
+++ ControleRecommands.Api/Program.cs
@@ -1,5 +1,13 @@
 using ControleRecomands.Infra.Context;
+using ControleRecomands.Infra.Repositories;
 using ControleRecomands.Infra.Repositories.UniteOfWork;
+using ControleRecommads.Domain.Commands;
+using ControleRecommads.Domain.Commands.Interfaces;
+using ControleRecommads.Domain.Entities;
+using ControleRecommads.Domain.Entities.ValueObject;
+using ControleRecommads.Domain.Handler;
+using ControleRecommads.Domain.Handler.Interface;
+using ControleRecommads.Domain.IRepositories;
 using ControleRecommads.Domain.IRepositories.IUniteOfWork;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,6 +21,12 @@ var connetions = builder.Configuration.GetConnectionString("connectionDb");
 
 builder.Services.AddDbContext<RecommendationDbContext>(options => options.UseSqlServer(connetions));
 builder.Services.AddTransient<IUniteOfWork, UniteOfWork>();
+builder.Services.AddTransient<IHandler<ReceiveCommand>, ReceivedRecommendationHandler>();
+builder.Services.AddTransient<IHandler<RetornRecommendationCommand>, ReceivedRecommendationHandler>();
+builder.Services.AddScoped<ICommandResult, CommandResult>(); 
+builder.Services.AddTransient<IRepositoryBase<ReceivedRecommendation>, RepositoryBase<ReceivedRecommendation>>();
+builder.Services.AddTransient<IEntityRepository<Member>, EntityRepository<Member>>();
+builder.Services.AddTransient<IEntityRepository<Church>,  EntityRepository<Church>>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
